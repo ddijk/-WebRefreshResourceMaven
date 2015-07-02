bc310feaea60452f5ae031af045fa028e6b487df
Dick Dijk <dick.dijk@ortec-finance.com>
2015-07-02 09:02:45 +0200

    working

--- src/main/java/nl/ortecfinance/opal/webrefreshresourcemaven/MyController.java
+++ src/main/java/nl/ortecfinance/opal/webrefreshresourcemaven/MyController.java
@@ -1,13 +1,20 @@
 package nl.ortecfinance.opal.webrefreshresourcemaven;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
 import org.apache.log4j.Logger;
 
 @ManagedBean
 public class MyController {
 
     private static final Logger LOGGER = Logger.getLogger(MyController.class);
+
+    String locale;
+
+    String browserLocale;
 
     String aap = "blah";
 
@@ -17,6 +24,23 @@ public class MyController {
 
     @PostConstruct
     public void init() {
-        LOGGER.info("Init");
+        LOGGER.info("Init, default locale is  " + Locale.getDefault());
     }
+
+    public String getLocale() {
+        locale = FacesContext.getCurrentInstance().getViewRoot().getLocale().toString();
+        return locale;
+    }
+
+    public String getBrowserLocale() {
+        browserLocale = FacesContext.getCurrentInstance().getExternalContext().getRequestLocale().toString();
+        return browserLocale;
+    }
+
+    public void clearCache() {
+        ResourceBundle.clearCache();
+
+        LOGGER.info("clear cache ...");
+    }
+
 }
